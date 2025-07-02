b3b9144f9699d2b1a4687d143148187d91a05189
Othman59 <othman.douz07@gmail.com>
2025-07-02 15:39:25 +0200

    Classe Guerrier créer

---
+++ Classes/Guerrier.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuelDeGuerrier.Classes
+{
+    internal class Guerrier
+    {
+        //Attributs
+
+        private string _nom; //Nom du guerrier
+        private int _pointsDeVie; //Pv
+        private int _nbDesAttaque; //Nombre de des attaques
+
+        public Guerrier(string nom, int pointsDeVie, int nbDesAttaque)
+        { //Constructeur
+            _nom = nom;
+            _pointsDeVie = pointsDeVie;
+            _nbDesAttaque = nbDesAttaque;
+        }
+
+        //Methode 
+
+        public string GetNom(int pointsDeVie)
+        {
+            return _nom;
+        }
+        public int GetPointsDeVie ()
+        {
+            return _pointsDeVie;
+        }
+        public void SetPointsDeVie(int pointsDeVie) //Paramètre
+        {
+            _pointsDeVie = pointsDeVie;    
+        }
+
+        public int GetNbDesAttaque()
+        {
+            return _nbDesAttaque;
+        }
+
+    }
+}
+
