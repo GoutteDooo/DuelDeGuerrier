268fa64fe038b59faaaf2b4cf8d999ed8fc9576d
GoutteDooo <goudal.Donovan@live.fr>
2025-07-03 14:36:01 +0200

    Partie3/Etape3(AfficherGuerrier): Switch ajouté, et navigation entre le menu principal et le sous-menu AjouterGuerrier fonctionnel

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ namespace DuelDeGuerrier
         {
             Console.WriteLine("--- Menu principal ---");
             Console.WriteLine("1. Créer un guerrier");
-            Console.WriteLine("2. Afficher un guerrier\n");
+            Console.WriteLine("2. Afficher un guerrier");
+            Console.WriteLine("3. Lancer un tournoi\n");
             Console.WriteLine("0. Quitter\n");
             Console.WriteLine("Veuillez entrer un nombre");
             string saisie = Console.ReadLine();
@@ -29,35 +30,51 @@ namespace DuelDeGuerrier
          */
         public static void AjouterGuerrier()
         {
-            while (true)
+            Console.Clear(); // Enlever tout le texte du menu principal
+            bool estActif = true; // Tant qu'il est sur true, le sous-menu est actif
+            while (estActif == true)
             {
-                Console.Clear();
-                Console.WriteLine($"---- Créer un Guerrier ----\n\n\n" +
-                    $"Quel type de fourmi souhaitez-vous créer ?\n\n" +
+                Console.WriteLine(
+                    $"---- Créer un Guerrier ----\n" +
+                    $"\n" +
+                    $"\n" +
+                    $"Quel type de fourmi souhaitez-vous créer ?\n" +
+                    $"\n" +
                     $"1. Fourmi Noire (Défense élevée)\n" +
-                    $"2. Fourmi Rousse (Attaque élevée)\n\n" +
-                    $"0. Quitter le sous-menu");
+                    $"2. Fourmi Rousse (Attaque élevée)\n" +
+                    $"\n" +
+                    $"0. Quitter le sous-menu\n");
                 Console.Write("> ");
                 string saisie = Console.ReadLine();
 
-                if (saisie == "1") // Si utilisateur veut créer une fourmi noire
-                {
-                    FourmiNoire fourmiTest = CreerFourmiNoire(); // On crée une nouvelle instance de FourmiNoire (nommée fourmiTest)
-                    Console.WriteLine("Une fourmi a été créée !");
-                    fourmiTest.AfficherInfos(); // On utilise la méthode AfficherInfos de la classe FourmiNoire
+                Console.Clear(); // Afficher le texte en lien avec la saisie et réafficher le sous-menu
+                Console.ForegroundColor = ConsoleColor.Green;
 
-                }
-                if (saisie == "2")
+                switch (saisie)
                 {
-                    FourmiRousse fourmiRousse = CreerFourmiRousse();
-                    Console.WriteLine("Une fourmi rousse a été !");
-                    fourmiRousse.AfficherInfos();
+                    case "1": // Si utilisateur veut créer une fourmi noire
+                        FourmiNoire fourmiTest = CreerFourmiNoire(); // On crée une nouvelle instance de FourmiNoire (nommée fourmiTest)
+                        Console.WriteLine("Une fourmi noire a été créée !");
+                        fourmiTest.AfficherInfos(); // On utilise la méthode AfficherInfos de la classe FourmiNoire
+                        break;
+
+                    case "2": // Si utilisateur veut créer une fourmi rousse
+                        FourmiRousse fourmiRousse = CreerFourmiRousse();
+                        Console.WriteLine("Une fourmi rousse a été créée !");
+                        fourmiRousse.AfficherInfos();
+                        break;
+
+                    case "0": // Si utilisateur veut quitter le sous-menu
+                        estActif = false;
+                        break;
+
+                    default:
+                        break;
                 }
-                break;
-            
-         
-                
+                Console.ResetColor();
             }
+            // Retourner au menu principal
+            AfficherMenuPrincipal();
         }
         /**
          * Retourne une instance de FourmiNoire
@@ -67,6 +84,9 @@ namespace DuelDeGuerrier
             return new FourmiNoire("pikachu",30,3,true);
         }
 
+        /**
+         * Retourne une instance de FourmiRousse
+         */
         public static FourmiRousse CreerFourmiRousse()
         {
             return new FourmiRousse("dracofeu", 50, 5);
