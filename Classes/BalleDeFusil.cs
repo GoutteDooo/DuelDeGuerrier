c11703715af4b46d0252b9db03da56ea4092e840
GoutteDooo <goudal.Donovan@live.fr>
2025-07-05 15:38:06 +0200

    P4(Sorcier/Attaquer()): Méthode redéfinie spécifique pour la classe Sorcier

--- Classes/BalleDeFusil.cs
+++ Classes/BalleDeFusil.cs
@@ -14,9 +14,47 @@ namespace DuelDeGuerrier.Classes
         private int _mana = 50;
         private List<string> sorts = new List<string> { "Boule de Feu", "Soin", "Bouclier Magique", "Tir à Bout Portant" };
         public int Mana { get; set; }
-        public BalleDeFusil(string nom, int pointsDeVie, int nbDesAttaque, int mana = 50) : base(nom, pointsDeVie, nbDesAttaque)
+        public bool BouclierActif { get; set; }
+        public BalleDeFusil(string nom, int pointsDeVie, int nbDesAttaque, int mana = 50, bool bouclierActif = false) : base(nom, pointsDeVie, nbDesAttaque)
         {
             Mana = mana;
+            BouclierActif = bouclierActif;
+        }
+
+        /**
+         * Méthode redéfinie de la classe mère Guerrier
+         */
+        public int Attaquer()
+        {
+            Random rng = new Random();
+            int degats = rng.Next(1, 6);
+            if (Mana >= 10)
+            {
+                // Lance un dés entre [0, sorts.Count - 1] pour récupérer un index aléatoire de sorts
+                int indexSort = rng.Next(0, sorts.Count - 1);
+                // On affiche le sort trouvé
+                Console.WriteLine($"{this.GetNom()} consomme 10 mana pour utiliser {sorts[indexSort]}!!");
+                /* SORTS */
+                switch (indexSort)
+                {
+                    case 0:
+                        Console.WriteLine("La Boule de Feu inflige 10 points de dégâts supplémentaires!!");
+                        degats += 10;
+                        break;
+                    case 1:
+                        Console.WriteLine($"{this.GetNom()} récupère 5 PV !");
+                        Console.WriteLine($"Test, PV avant : {this.PointsDeVie}");
+                        this.PointsDeVie += 5;
+                        Console.WriteLine($"Test, PV après : {this.PointsDeVie}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"Un bouclier Magique s'active, réduisant les dégâts subits de 50% contre la prochaine attaque.");
+                        BouclierActif = true;
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
